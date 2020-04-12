7dba07349772affed081b0b5ba2ce569f401593e
hemanth <hemanththyagaraj@gmail.com>
2020-04-13 00:00:45 +0530

    removing firebase config

--- src/firebase.utils.js
+++ src/firebase.utils.js
@@ -3,14 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2BLx_VbUsAH5vp6gpllRsitpiIqOmYfU",
-  authDomain: "todo-tracker-d43a8.firebaseapp.com",
-  databaseURL: "https://todo-tracker-d43a8.firebaseio.com",
-  projectId: "todo-tracker-d43a8",
-  storageBucket: "todo-tracker-d43a8.appspot.com",
-  messagingSenderId: "710642039188",
-  appId: "1:710642039188:web:822154ccc516e7866d3781",
-  measurementId: "G-LT94VX8J18",
+  
 };
 
 firebase.initializeApp(firebaseConfig);
